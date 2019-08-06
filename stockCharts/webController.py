182cda4022a084b0a22d7a78b14d8adce28cdbfb
Harry <a1689938@student.adelaide.edu.au>
2019-08-06 15:11:32 +0900

    commit

--- stockCharts/webController.py
+++ stockCharts/webController.py
@@ -246,6 +246,6 @@ def check_session():
     return user_id.decode('utf8', 'ignore')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True, host='0.0.0.0')
 #     app.run(debug=True)
 #     app.run('192.168.123.131',port='5000', debug=True)
